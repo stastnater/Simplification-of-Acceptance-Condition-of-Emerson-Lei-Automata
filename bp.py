07f026180c5ddc5203edd47da23a8a65655cac22
xstastna <xstastna@fi.muni.cz>
2019-09-20 23:18:52 +0200

    Added auxiliary functions, simplification in progress

--- bp.py
+++ bp.py
@@ -3,11 +3,12 @@ import enum
 spot.setup()
 
 ### ACC CLASS ###
-class MarkType(enum.Enum):
+class MarkType(enum.Enum): #TODO: docu
     Inf = 1
     Fin = 2
 
-class ACCMark:
+
+class ACCMark: #TODO: docu
     def __init__(self, mtype, num):
         self.type = mtype
         self.num = num
@@ -16,7 +17,7 @@ class ACCMark:
         return ''.join(["Inf" if self.type == MarkType.Inf else "Fin", '(', str(self.num), ')'])
 
 
-class PACC:
+class PACC: #TODO: docu
     def __init__(self, acc):
         self.formula = parse_acc(acc)
 
@@ -33,9 +34,70 @@ class PACC:
                 f.append(" | ")
         return ''.join(f)
 
+    def int_format(self):
+        f = []
+        for dis in self.formula:
+            d = []
+            for con in dis:
+                d.append(con.num)
+            f.append(d)
+        return f
+
+    def clean_up(self, aut, scc):
+        clean_f = []
+        marks = scc_current_marks(aut, scc)
+        for dis in self.formula:
+            clean_dis = []
+            for con in dis:
+                if con.num in marks:
+                    clean_dis.append(con)
+            if not clean_dis:
+                clean_f.append(clean_dis)
+        self.formula = clean_f
+
+    def get_mtype(self, m):
+        for dis in self.formula:
+            for con in dis:
+                if m == con.num:
+                    return con.type
+
+    def find_m(self, m): #TODO: delete
+        occurrences = []
+        for i in range(len(self.formula) - 1):
+            for j in range(len(self.formula[i]) - 1):
+                if self.formula[i][j].num == m:
+                    occurrences.append(i,j)
+        return occurrences
+
+    def find_m_dis(self, m):
+        occurrences = []
+        for i in range(len(self.formula) - 1):
+            for j in range(len(self.formula[i]) - 1):
+                if self.formula[i][j].num == m:
+                    occurrences.append(i)
+        return occurrences
+
 
 ### PARSE ACC ###
+
 def parse_acc(acc):
+    """
+    Parses an acc in DNF and returns the formula represented by list of lists of ACCMarks. 
+    The inner lists represent disjuncts of the formula. 
+    The inner lists contain ACCMark (see ACCMark class documentation) objects representing atomic conditions (such as Inf(1)).
+
+    Example: (Fin(1) & Inf(2)) | (Inf(3)) | (Fin(1) & Fin(4)) --> [[ACCMark(2,1), ACCMark(1,2)] [ACCMark(1,3)] [ACCMark(2,1), ACCMark(2,4)]]
+
+    Parameters
+    ----------
+    aut : spot::acc_cond::acc_code
+
+    Returns
+    -------
+    [[ACCMark]]
+        List of lists of ACCMarks.
+    """
+
     formula = []
     for dis in str(acc).split('|'):
         new_dis = []
@@ -52,23 +114,53 @@ def parse_acc(acc):
     return formula
 
 
-### SIMPLIFY ###
+### SIMPLIFY AUXILIARY ###
 
-def count_occur(acc, m): #count how many times mark m occurs in acc
+def acc_count_occur(acc, m): 
+    """
+    Counts the occurrences of mark m in given acceptance condition.
+
+    Parameters
+    ----------
+    acc : PACC
+    m : int
+        Number of an acceptance mark
+
+    Returns
+    -------
+    int
+        Amount of occurrences of m in acc.
+    """
+
     occur = 0
-    for c in str(acc):
-        if c == str(m):
-            occur += 1
+    for dis in acc:
+        for con in dis:
+            if con.num == m:
+                occur += 1
     return occur
 
 
-def scc_current_marks(aut, scc): #return a list of marks currently present on edges in given scc
+def scc_current_marks(aut, scc): 
+    """
+    Return a list of marks currently present on edges in given scc.
+
+    Parameters
+    ----------
+    aut : spot::twa        
+    scc : spot::scc_info_node 
+
+    Returns
+    -------
+    [int]
+        List of marks on edges of given scc.
+    """
+
     marks = []
     for s in scc.states():
         for e in aut.out(s): 
             for m in e.acc.sets():
                 if m not in marks:
-                    marks.append(m)
+                    marks.append(int(m)) 
     return marks
 
 
@@ -96,14 +188,79 @@ def scc_compl_marks(aut, scc): #return array of tuples of complementary marks in
                     c_marks.append((m1, m2))
     return c_marks
 
+
+def scc_subsets(aut, scc):
+    """
+    Return a list of tuples (m1, m2) containing acceptance marks m1 and m2 where m2 is a subset of m1 in given scc.
+
+    Parameters
+    ----------
+    aut : spot::twa        
+    scc : spot::scc_info_node 
+
+    Returns
+    -------
+    [(int, int)]
+        List of tuples of subsets.
+    """
+
+    marks = scc_current_marks(aut, scc)
+    subsets = []
+    for m1 in marks:
+        for m2 in marks:
+            if m1 is not m2:
+                is_sub = True
+                for s in scc.states():
+                    for e in aut.out(s):
+                        if e.dst in scc.states() and e.acc.has(m2) and not e.acc.has(m1):
+                            is_sub = False
+                if is_sub:
+                    subsets.append((m1, m2))
+    return subsets
+    
+
+def simpl_inf_con(autacc, scc, subsets):
+    int_acc = autacc[1].int_format()
+    for sub in subsets:
+        if autacc[1].get_mtype(sub[0]) == 1 and autacc[1].get_mtype(sub[1]) == 1:
+            if autacc[1].find_m_dis(sub[0]) == autacc[1].find_m_dis(sub[1]):
+            #TODO: remove all sub[0] marks from aut, clean acc, ret autacc
+                pass
+            else:
+                pass #remove sub[0] at least from dis where it is with sub[1]
+        
+                
+
+
+
+def simpl_subsets(aut, scc, acc):
+    subsets = scc_subsets(aut, scc)
+    autacc = (aut, acc)
        
 def simplify_compl(aut, scc):
     for cm in scc_compl_marks(aut, scc):
-        pass #TODO
+        pass #TODO:
+
+
+
+### SIMPLIFY ###
+
+def simplify(aut, scc, acc):
+    
+    pass
 
 ### MERGE ACCs ###
 
 ### MAIN ###
+
+def main():
+    aut = spot.automaton('necofile.aut')
+    scc_accs = [aut.get_acceptance().to_dnf()]*(spot.scc_info(aut).scc_count())
+    acc_index = 0
+
+    for scc in spot.scc_info(aut):
+        simplify(aut, scc, acc_index)
+        acc_index += 1
 
 
 ### PARSE TESTS ###
