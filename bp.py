d0911b7b3581dba4463849b28c687b13fa9d49d4
xstastna <xstastna@fi.muni.cz>
2019-10-21 15:46:33 +0200

    make_equiv in progress

--- bp.py
+++ bp.py
@@ -200,7 +200,6 @@ def scc_clean_up_edges(aut, acc, scc):
         for e in aut.out(s):
             for m in e.acc.sets():
                 if int(m) not in current_m:
-                    print("REMOVING mark from edge: ", m)
                     e.acc.clear(m)
 
 
@@ -440,6 +439,23 @@ def simplify(aut, acc, scc):
 
 ### MERGE AUXILIARY ###
 
+def shift_fst_acc(aut, acc, scc):
+    next_m = aut.acc().num_sets()
+    log = {}
+
+    for dis in acc.formula:
+        for con in dis:
+            if con.num in log:
+                con.num = log[con.num]
+            else:
+                add_dupl_marks(aut, scc, con.num, next_m)
+                log[con.num] = next_m
+                con.num = next_m
+                next_m += 1
+    scc_clean_up_edges(aut, acc, scc)
+    return log
+
+
 def count_cost(dis1, dis2):
     inf, fin = 0, 0
     for con in dis1:
@@ -482,7 +498,7 @@ def replace_marks(aut, scc, origin_m, new_m):
 
 
 def partition(accs, sccs, low, high): 
-    i = (low - 1)         # index of smaller element 
+    i = (low - 1) 
     pivot = len(accs[high])
   
     for j in range(low , high):   
@@ -503,29 +519,81 @@ def acc_quicksort(accs, sccs, low, high):
         acc_quicksort(accs, sccs, low, pi - 1) 
         acc_quicksort(accs, sccs, pi + 1, high) 
 
+
 def place_con(dis, con, used):
     if con in dis and not used[dis.index(con)]:
         return dis.index(con)
     for i in range(len(dis)):
-        if dis[i].type == con.type and not used[i]:
+        if dis[i].type == con.type and not used[i]:            
             return i
     return None
         
-def resolve_dependencies(aut, acc1, scc1, acc2):
+
+def add_dupl_marks(aut, scc, origin_m, new_m):
+    for s in scc.states():
+        for e in aut.out(s):
+            if e.dst in scc.states() and e.acc.has(origin_m):
+                if not e.acc.has(new_m):
+                    e.acc.set(new_m)
+
+
+def get_dependencies(acc):
     depend = []
-    int_f = acc1.int_format()
-    for i in range(len(int_f)):
+    int_f = acc.int_format()
+    for i in range(len(int_f)):        
         for j in range(len(int_f)):
             intersect = list(set(int_f[i]).intersection(set(int_f[j])))
             if i != j and intersect:
                 for mark in intersect:
-                    depend.append((mark, i, j))
+                    if any(d[0].num == mark for d in depend):
+                        for d in depend:
+                            if d[0].num == mark:
+                                if i not in d[1:]:
+                                    d.append(i)
+                                if j not in d[1:]:
+                                    d.append(j)                                    
+                    else:
+                        depend.append([ACCMark(acc.get_mtype(mark), mark), i, j])
+    return depend
+
+def resolve_dependencies(aut, acc1, scc1, acc2, scc2):
+    acc1_d = get_dependencies(acc1)
     m = make_matrix(acc1, acc2)
     row_ind, col_ind = linear_sum_assignment(m) 
-    
-
-
-
+    s_acc2 = []
+    for ci in col_ind:
+        if ci < len(acc2):
+            s_acc2.append(acc2[ci])
+        else: 
+            s_acc2.append([])
+    acc2_d = get_dependencies(PACC(str(s_acc2)))
+    for d1 in acc1_d: #TODO: sort acc1_d by length for better results
+        for d2 in acc2_d:
+            # dependencies needn't be removed
+            if d1[0].type == d2[0].type and all(d1[i] in d2 for i in range(1, len(d1))):
+                add_dupl_marks(aut, scc2, d2[0].num, d1[0].num) #TODO: possibly replace, not duplicate
+                """
+                for s in scc2.states():
+                    for e in aut.out(s):
+                        if e.acc.has(d2[0].num):
+                            e.acc.set(d1[0].num) 
+                """
+                for dis in acc2:
+                    for con in dis:
+                        if con == d2[0]:
+                            con.num = d1[0].num           
+                d2 = []
+                d1 = []
+                break 
+    for d1 in acc1_d:
+        if d1:
+            for i in range(2, len(d1)):
+                for con in acc1[d1[i]]:
+                    if con == d1[0]:
+                        new_m = acc1.max() + 1
+                        add_dupl_marks(aut, scc1, con.num, new_m)
+                        con.num = new_m
+                                                         
 
 ### MERGE ###
 
@@ -536,8 +604,7 @@ def merge(aut, acc1, scc1, acc2, scc2):
     row_ind, col_ind = linear_sum_assignment(m) 
     print("row ind: ", row_ind, " col ind: ", col_ind)
 
-    log = []
-    m_log = []
+    log = [scc2]
     for i in range(len(row_ind)):
         dis1 = acc1[col_ind[i]]
         used = [False] * len(dis1)
@@ -548,15 +615,16 @@ def merge(aut, acc1, scc1, acc2, scc2):
         for con in dis2:
             index = place_con(dis1, con, used)
             if index is None:
-                m_log.append((None, acc1.max() + 1))
+                log.append((con.num, acc1.max() + 1))
                 dis1.append(ACCMark(con.type, acc1.max() + 1))
                 used.append(True)
             else:
                 log.append((con.num, dis1[index].num))
                 replace_marks(aut, scc2, con.num, dis1[index].num)
                 used[index] = True
+    return log
                         
-def merge_accs(aut, sccs, accs): #TODO:
+def merge_accs(aut, sccs, accs):
     nempty_accs = []
     nempty_sccs = []
 
@@ -567,27 +635,74 @@ def merge_accs(aut, sccs, accs): #TODO:
 
     acc_quicksort(nempty_accs, nempty_sccs, 0, len(nempty_accs) - 1)
     
-    ### TODO: REMOVE
-    for acc in nempty_accs:
-        print(acc)
-    ###
+    merged_f = PACC(str(nempty_accs[0]))
+    merged_log = shift_fst_acc(aut, merged_f, nempty_sccs[0])
     
-    merged_f = copy.deepcopy(nempty_accs[0])
-
-    """
     for dis1 in merged_f.int_format():
         for dis2 in merged_f.int_format():
             if dis1 != dis2 and list(set(dis1).intersection(set(dis2))):
-                for i in range(1, len(nempty_accs) - 1):
-                    resolve_dependencies(aut, merged_f, nempty_sccs[0], nempty_accs[i])
+                for i in range(1, len(nempty_accs)):
+                    resolve_dependencies(aut, merged_f, nempty_sccs[0], nempty_accs[i], nempty_sccs[i])
                 break
-    """
-    log = []
+    
+    logs = []
     for i in range(1, len(nempty_accs) - 1):
-        merge(aut, merged_f, nempty_sccs[0], nempty_accs[i], nempty_sccs[i])
-    print(merged_f)
-            
+        logs.append(merge(aut, merged_f, nempty_sccs[0], nempty_accs[i], nempty_sccs[i]))
+    return merged_f, merged_log, logs
 
+
+### MAKE NEW AUT EQUIVALENT ###
+
+def make_false(aut, scc, merged_acc):
+    for s in scc.states(): #TODO: clean up instead?
+        for e in aut.out(s):
+            if e.dst in scc.states():
+                for m in e.acc.sets():
+                    e.acc.clear(m)
+    add_m = []
+    for dis in merged_acc.formula:
+        if all(con.type == MarkType.Fin for con in dis):
+            add_m.append(dis[0].num)
+    if not add_m:
+        return
+    for s in scc.states():
+        for e in aut.out(s):
+            if e.dst in scc.states():
+                for m in add_m:
+                    e.acc.set(m)
+
+def make_equiv(aut, accs, sccs, log, logs, merged_acc):
+    print(log)
+    for i in range(1, len(accs)):
+        if not accs[i]: 
+            make_false(aut, sccs[i], merged_acc)
+        else:
+            contained = []
+            for dis in accs[i].formula:
+                for con in dis:
+                    if con not in contained:
+                        contained.append(con)
+            for l in logs: 
+                if l[0] is sccs[i]:
+                    for m in contained:
+                        m.num = l[m.num]   
+                    break     
+            add_m = []
+            for dis in merged_acc:
+                if all(con not in contained for con in dis): #whole disjunct to be made false
+                    if all(con.type == MarkType.Fin for con in dis):
+                        if all(con.num not in add_m for con in dis):
+                            add_m.append(dis[0].num)
+                else: # all atomic conditions not present in disjunct to be made true
+                    for con in dis:
+                        if con not in contained and con.type == MarkType.Inf:
+                            add_m.append(con.num)
+            for s in sccs[i].states():
+                for e in aut.out(s):
+                    if e.dst in sccs[i].states():
+                        for m in add_m:
+                            e.acc.set(m)
+        scc_clean_up_edges(aut, merged_acc, sccs[i])
 
 ### MAIN ###
 
@@ -605,9 +720,14 @@ def main(argv):
         simplify(aut, acc, scc)
         accs.append(acc)
 
-    merge_accs(aut, sccs, accs)
+    new_acc, log, logs = merge_accs(aut, sccs, accs)
+    aut.set_acceptance(new_acc.max() + 1, spot.acc_code(str(new_acc)))
+
+    make_equiv(aut, accs, sccs, log, logs, new_acc)
     
     aut.save('_' + FILENAME)
+    print(new_acc)
+    
 
 
 """
